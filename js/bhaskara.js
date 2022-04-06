8f987d7e9d5d9ba41ab4cad13754d0e6b025fdf4
Brenon Paul <bp.brenon@gmail.com>
2022-04-06 10:37:56 -0300

    Mudança na fonte e na apresentação do resultado no HTML

--- js/bhaskara.js
+++ js/bhaskara.js
@@ -6,7 +6,7 @@ function imprimir(){
     const c = document.getElementById("c").value
     
     const result = bhaskara(a, b, c)
-    document.getElementById("res").innerHTML = "<h2><b>S = </b>{"+result.x1.toFixed(2) + ", "+result.x2.toFixed(2)+"}</h2>"
+    document.getElementById("res").innerHTML = "<h2><b>S = </b>{"+(typeof(result.x1) == "number"? result.x1.toFixed(2): result.x1 )+ ", "+(typeof(result.x2) == "number"?result.x2.toFixed(2): result.x2)+"}</h2>"
 }
 
 document.getElementById("botao").addEventListener("click", imprimir);
